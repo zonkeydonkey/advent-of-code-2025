2408f65936eb7d79211cc21da48b22f98056bfd3
zonkeydonkey <sylwia.blach404@gmail.com>
2025-12-01 22:55:57 +0100

    Puzzle 1:2

--- lib/puzzle_01_2.exs
+++ lib/puzzle_01_2.exs
@@ -12,37 +12,34 @@ defmodule Puzzle01_2 do
     |> Enum.reduce({0, 50}, fn row, {counter, position} ->
       <<direction::binary-size(1), rest::binary>> = row
       distance = parse_int!(rest)
+      full_circles_count = Integer.floor_div(distance, 100)
       effective_distance = rem(distance, 100)
       case direction do
         "L" ->
           base = position - effective_distance
-          next_position = cond do
+          {next_position, zero_clicked?} = cond do
             base < 0 ->
-              100 + base
+              {100 + base, position != 0}
+            base == 0 and effective_distance != 0 ->
+              {base, true}
             true ->
-              base
+              {base, false}
           end
 
-          if next_position == 0 do
-            {counter + 1, next_position}
-          else
-            {counter, next_position}
-          end
+          {counter + full_circles_count + if(zero_clicked?, do: 1, else: 0), next_position}
 
         "R" ->
           base = position + effective_distance
-          next_position = cond do
+          {next_position, zero_clicked?} = cond do
             base > 99 ->
-              base - 100
+              {base - 100, position != 0}
+            base == 0 and effective_distance != 0 ->
+              {base, true}
             true ->
-              base
+              {base, false}
           end
 
-          if next_position == 0 do
-            {counter + 1, next_position}
-          else
-            {counter, next_position}
-          end
+          {counter + full_circles_count + if(zero_clicked?, do: 1, else: 0), next_position}
       end
     end)
   end
